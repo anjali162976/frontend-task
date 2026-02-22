51fc4fcc4f01c67219ef1c6c911b57df00a26c2d
anjali162976 <anjaligavhane014@gmail.com>
2026-02-23 01:51:02 +0530

    Update Process.jsx

--- src/components/Process.jsx
+++ src/components/Process.jsx
@@ -15,7 +15,7 @@ function Process() {
 
 <section className="w-[100%] mx-auto my-12">
 
-  {/* First Box (Active) */}
+  {/* First Box  */}
  <div className="relative bg-[#e5e5e5] md:bg-[#B9FF66] rounded-[40px] border-2 border-black shadow-[0_8px_0_black] p-6 md:p-10 mb-8 overflow-hidden">
 
     <div className="hidden md:block w-[90%] h-[2px] bg-black mb-6"></div>
@@ -29,7 +29,7 @@ function Process() {
 
     <div className="flex items-center justify-between">
 
-  {/* LEFT: Text (same as 2nd box) */}
+  {/* LEFT */}
   <div className="flex items-center gap-6">
     <h1 className="hidden md:block text-[60px] font-bold">01</h1>
     <h2 className="text-2xl md:text-3xl font-semibold">
@@ -37,7 +37,7 @@ function Process() {
     </h2>
   </div>
 
-  {/* RIGHT: Button */}
+  {/* RIGHT */}
   <div className="w-[46px] h-[46px] md:w-[60px] md:h-[60px] rounded-full bg-gray-300 border-2 border-black flex items-center justify-center text-2xl md:text-3xl font-bold">
     <span className="md:hidden">+</span>
     <span className="hidden md:block">−</span>
@@ -46,7 +46,7 @@ function Process() {
 </div>
     </div>
 
-  {/* Second Box (Closed) */}
+  {/* Second Box  */}
  <div className="relative bg-[#e5e5e5] md:bg-[#B9FF66] rounded-[40px] border-2 border-black shadow-[0_8px_0_black] p-6 md:p-10 mb-8 overflow-hidden">
 
    <div className="pr-20 md:pr-0">
@@ -57,7 +57,7 @@ function Process() {
     </h2>
   </div>
 
-  {/* Button pinned inside box */}
+  {/* Button */}
   <div className="absolute right-6 top-1/2 -translate-y-1/2">
     <div className="w-[46px] h-[46px] md:w-[60px] md:h-[60px] rounded-full bg-gray-300 border-2 border-black flex items-center justify-center text-2xl md:text-3xl font-bold">
       +
